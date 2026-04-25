ca1868db0eb062650e157a5ed36cf405d8922822
Nicolly <saantos.nick1606@gmail.com>
2026-04-25 16:37:16 -0300

    feat: exercicios com phyton

---
+++ 02-boas_vindas.py
@@ -0,0 +1,4 @@
+nome = input("Qual é o seu nome?")
+
+print(f"Seja bem-vindo(a){nome}")
+print("Seja bem-vindo(a){nome}")
---
+++ 04-feira.py
@@ -0,0 +1,5 @@
+print("Seja bem-vindo a Feirinha 🍒!")
+print("")
+melancia = int(input("Quantas melancias você vai querer?"))
+total = melancia*20
+print(f"O valor do seu pedido foi de R${total}")
